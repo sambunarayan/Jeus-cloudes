1faf7c894f6d27c1d46994798f6e88e6c1fce384
sambunarayan <soyou30257@gmail.com>
2021-09-13 23:10:37 +0900

    20210913 Build jeus-k8s server
    ・Added initialize shell files of helm and jenkins.

--- jeus-k8s-cloud/helm/init-helm.sh
+++ jeus-k8s-cloud/helm/init-helm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # install helm
-export DESIRED_VERSION=v3.2.1; /root/Jeus-cloudes/jeus-k8s-cloud/helm/helm-install.sh
+export DESIRED_VERSION=v3.2.1; $HOME/Jeus-cloudes/jeus-k8s-cloud/helm/helm-install.sh
 
 # add helm chart repository
 helm repo add edu https://iac-source.github.io/helm-charts
