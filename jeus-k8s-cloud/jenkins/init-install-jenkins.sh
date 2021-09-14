428e8c3762a159d4f6ea43037420545c5799541d
sambunarayan <soyou30257@gmail.com>
2021-09-14 23:04:32 +0900

    20210914 Build jeus-k8s server
    ・Edited jenkins install shell.
      added nfs name parameter.

--- jeus-k8s-cloud/jenkins/init-install-jenkins.sh
+++ jeus-k8s-cloud/jenkins/init-install-jenkins.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Create nfs directory for jenkins
-sh $HOME/Jeus-cloudes/jeus-k8s-cloud/jenkins/nfs-exporter.sh
+sh $HOME/Jeus-cloudes/jeus-k8s-cloud/jenkins/nfs-exporter.sh jenkins
 
 # Set jenkins volume
 kubectl apply -f $HOME/Jeus-cloudes/jeus-k8s-cloud/jenkins/jenkins-volume.yaml
