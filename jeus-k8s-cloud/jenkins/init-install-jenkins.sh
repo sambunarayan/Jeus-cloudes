04f63c25162272544a026ae74ebd93b3414f8446
sambunarayan <soyou30257@gmail.com>
2021-09-18 22:27:04 +0900

    20210918 Build jeus-k8s

--- jeus-k8s-cloud/jenkins/init-install-jenkins.sh
+++ jeus-k8s-cloud/jenkins/init-install-jenkins.sh
@@ -3,6 +3,7 @@
 # Create nfs directory for jenkins
 sh $HOME/Jeus-cloudes/jeus-k8s-cloud/jenkins/nfs-exporter.sh jenkins
 
+# Chnage permission
 chown 1000:1000 /nfs_shared/jenkins/
 
 # Set jenkins volume
