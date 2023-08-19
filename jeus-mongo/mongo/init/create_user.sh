9058ad815e8f1ac1a3fc2180cd299b5e837e1dc1
Jaewoo Kim <soyou30257@gmail.com>
2023-08-19 16:02:48 +0900

    Edit user config for mongoDB

---
+++ jeus-mongo/mongo/init/create_user.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+query=$(cat <<EOS
+use appdb\n
+db.createUser({user: "sampleuser", pwd: "sampleuser", roles: [{role:"readWrite",db: "appdb"}]})\n
+
+echo -e "\n" $query | mongo -u $MONGO_INITDB_ROOT_USERNAME -p$MONGO_INITDB_ROOT_PASSWORD
