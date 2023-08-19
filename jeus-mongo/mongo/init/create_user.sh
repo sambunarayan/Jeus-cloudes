df1d13565b2311abe318d68be572692fa11ef119
Jaewoo Kim <soyou30257@gmail.com>
2023-08-19 16:06:13 +0900

    Edit create user shell for mongoDB

--- jeus-mongo/mongo/init/create_user.sh
+++ jeus-mongo/mongo/init/create_user.sh
@@ -3,5 +3,7 @@
 query=$(cat <<EOS
 use appdb\n
 db.createUser({user: "sampleuser", pwd: "sampleuser", roles: [{role:"readWrite",db: "appdb"}]})\n
+EOS
+)
 
 echo -e "\n" $query | mongo -u $MONGO_INITDB_ROOT_USERNAME -p$MONGO_INITDB_ROOT_PASSWORD
